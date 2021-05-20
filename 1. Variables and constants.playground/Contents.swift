89413918220e879d839b9f9df81a480637269432
datmcl <mixail.bokov9@gmail.com>
2021-05-20 15:13:19 +0700

    add variables and constants

---
+++ 1. Variables and constants.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+// ---------------------|
+//                      |
+// Variables & constants|
+//                      |
+// ---------------------|
+
+// Let is a constant, we can't change it
+let name = "Steve"
+// Age is a var(iable), so we can change it's value
+var age = 18
+
+// If I try to do this, I'll get an error
+// name = "Tim"
+
+// Let's change it
+age = 21
