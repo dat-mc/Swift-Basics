7da316c176ebe17a26a5a7a77414b9c55df65484
datmcl <mixail.bokov9@gmail.com>
2021-05-20 23:15:17 +0700

    add data types

---
+++ 2. Data types.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+// -----------|
+//            |
+// Data types |
+//            |
+// -----------|
+
+// Character is a single character (thanks, cap)
+let emoji: Character = "😺"
+
+//String is sequence of characters
+let str: String = "Hello, World!"
+
+// Int is an integer number (thanks, cap x2)
+let number: Int = 42
+
+// Float is a number with floating point (8 decimal places)
+let floatNumber: Float = 1.123_456_78
+
+// Double is a number with floating point (16 decimal places)
+let pi: Double = .pi
+
+// Bool can be true or false
+let flag: Bool = true
