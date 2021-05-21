e7be96acc2f3a579cb1d1e095282211888f1de59
datmcl <mixail.bokov9@gmail.com>
2021-05-21 15:51:41 +0700

    add string interpolation

---
+++ 4. String interpolation.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+// ---------------------|
+//                      |
+// String interpolation |
+//                      |
+// ---------------------|
+
+let name = "Steve"
+var age = 21
+
+let string = "My name is \(name). I'm \(age)"
+let anotherString = "\(100 - 50)"
