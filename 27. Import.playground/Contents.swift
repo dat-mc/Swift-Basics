48db816dad290fe53ef94ba112240943e62d6cbc
datmcl <mixail.bokov9@gmail.com>
2021-06-18 15:15:06 +0700

    add import

---
+++ 27. Import.playground/Contents.swift
@@ -0,0 +1,14 @@
+// -------|
+//        |
+// Import |
+//        |
+// -------|
+
+import UIKit
+import Foundation
+
+let string = "Hello, world!"
+
+// Import is used to import libraries / frameworks
+// e.g. if you comment the libraries above, this method will not be found at scope
+string.components(separatedBy: " ")
